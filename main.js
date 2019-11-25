faafd824e75e9b6e19f94165ab9fd1503d848276
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-11-25 16:32:12 +0100

    toggleClass aggiunto.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 
-$('.fa-times').addClass("menu-cell");
-$('.fa-bars').addClass("menu-cell");
 
 $('.menu-cell').click(function(){
-    $('.hamburger-menu').toggle(0);
+    $('.hamburger-menu').toggleClass("active");
 })
