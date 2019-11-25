9c9942d9c3f6e30c2e91b078bbde16eb5236537b
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-11-25 16:11:05 +0100

    aggiunta classe

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
-$('i.menu-cell').click(function(){
+$('.fa-times').addClass("menu-cell");
+$('.fa-bars').addClass("menu-cell");
+
+$('.menu-cell').click(function(){
     $('.hamburger-menu').toggle(0);
 })
